2afc318b2804e146f08b102bd6333eb186b11c8e
Derek Kwan <derek.x.kwan@gmail.com>
2018-07-17 18:14:30 -0700

    finish 1_1

---
+++ README.md
@@ -0,0 +1,8 @@
+# cryptopals-solutions
+
+solutions to the [cryptopals](https://cryptopals.com/) crypto challenges in Racket organized by problem set
+
+- filenames take the format `x_y.rkt` where x is the problem set and y is the challenge number
+
+## finished so far
+**set 1** - problem 1
---
+++ set_1/1_1.rkt
@@ -0,0 +1,59 @@
+#lang racket
+
+
+(define (hexchar->integer c)
+  (cond [(char<=? #\0 c #\9) (- (char->integer c) (char->integer #\0))]
+	[(char<=? c #\f) (+ 10 (- (char->integer c) (char->integer #\a)))]
+	[else 0]))
+
+(define (hex->bytes s)
+  (let* ([cleaned (string-downcase s)]
+	 [valid-hex (regexp-match? #px"^([[:xdigit:]]{2})*$" cleaned)])
+
+    (if (and valid-hex (even? (string-length cleaned)))
+	(apply bytes (for/list ([i (range (/ (string-length cleaned) 2 ))])
+	  (+ (* 16 (hexchar->integer (string-ref cleaned (* 2 i))))
+	     (hexchar->integer (string-ref cleaned (+ (* 2 i) 1))))))
+	(raise-argument-error "improper format")
+	)))
+
+(define (integer->b64char i)
+  (cond [(<= 0 i 25) (integer->char (+ (char->integer #\A) i))]
+	[(<= 26 i 51) (integer->char (+ (char->integer #\a) (- i 26)))]
+	[(<= 52 i 61) (integer->char (+ (char->integer #\0) (- i 52)))]
+	[(= 62 i) #\+]
+	[(= 63 i)  #\/]
+	[else #\=]))
+
+(define (bytes->six-bit-list bytstr)
+  ;;assume we are being passeed <= 3 byte chunks
+  ;; let 64 be the padding char
+  (let* ([bn (bytes-length bytstr)]
+	 [padn (max (- 3 bn) 0)] 
+	 [char1 (bytes-ref bytstr 0)]
+	 [char2 (if (> bn 1) (bytes-ref bytstr 1) 0)]
+	 [char3 (if (> bn 2) (bytes-ref bytstr 2) 0)]
+	 [b64-1 (arithmetic-shift char1 -2)]
+	 [b64-2 (+ (arithmetic-shift (bitwise-and char1 3) 4)
+		   (arithmetic-shift char2 -4))]
+	 [b64-3 (+ (arithmetic-shift (bitwise-and char2 15) 2)
+		   (arithmetic-shift char3 -6))]
+	 [b64-4 (+ (bitwise-and char3 63))])
+    (cond [(= padn 0) (list b64-1 b64-2 b64-3 b64-4)]
+	  [(= padn 1) (list b64-1 b64-2 b64-3 64)]
+	  [else (list b64-1 b64-2 64 64)])
+    ))
+	 
+(define (bytes->b64 bstr)
+  (apply string-append (for/list ([i (range (floor (/ (bytes-length bstr) 3)))])
+    (let ([cur-sub (subbytes bstr (* 3 i) (* 3 (+ i 1)))])
+       (list->string (map integer->b64char (bytes->six-bit-list cur-sub)))
+      ))))
+
+
+
+(define hex-input "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+
+(define result (bytes->b64 (hex->bytes hex-input)))
+
+(println result)
