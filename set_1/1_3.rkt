c0ed8d4e9146c436096fd7b33575f36b58506364
Derek Kwan <derek.x.kwan@gmail.com>
2018-07-22 07:24:19 -0700

    cleaning code

--- set_1/1_3.rkt
+++ set_1/1_3.rkt
@@ -21,32 +21,10 @@
     (apply + (map (lambda (n) (hash-ref my-scores (integer->char n))) filtered))
     ))
 
-(define (list-count lst)
-  (let ([myhash (make-hash)])
-    (for/list ([i lst])
-      (if (hash-has-key? myhash i)
-	  (hash-update! myhash i (lambda (n) (+ 1 n)))
-	  (hash-set! myhash i 1)))
-    myhash))
-
-(define (sort-hash hsh)
-  (let ([hash-list (hash->list hsh)])
-    (sort hash-list (lambda (x y) (> (cdr x) (cdr y))))
-    ))
-  
-(define (list-pad lst [n 0] #:padval [padval '()])
-  (let ([len (length lst)]
-	[padn (max n 0)])
-    (if (> padn len)
-      (append lst (build-list (- padn len) (lambda (n) padval)))
-      lst)
-    ))
-      
-
 (let* ([decoded (bytes->list (hex->bytes "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"))])
   (for/fold ([best-score 0]
 	     [best-str #"unscored"]
-	     #:result best-str)
+	     #:result (bytes->string/utf-8 best-str))
       ([i (range 256)])
     (let* ([xored (map (lambda (n) (bitwise-xor n i)) decoded)]
 	   [cur-str (list->bytes xored)]
